b0ca93643e8e655dcb238744b3332bb686ddecb2
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-02-25 18:44:09 +0500

    background

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -4,7 +4,6 @@ import {getRandomColor} from '../utils'
 export class BackgroundModule extends Module {
     trigger() {
         document.body.style.backgroundColor = getRandomColor()
-        const greeting = document.querySelector('.greeting')
-        greeting.style.display = 'none'
+        
     }
 }
